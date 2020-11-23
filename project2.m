607a7c19e42433b3b29996c9d86459c84a03736e
Sean Hwang <hwang103@purdue.edu>
2020-11-23 03:21:42 -0500

    DFT for a setup

--- project2.m
+++ project2.m
@@ -10,3 +10,28 @@ clear all
 close all 
 
 % A) h_0 = {1/sqrt(2), 1/sqrt(2)} ,, h_1 = {1/sqrt(2), -1/sqrt(2)} 
+
+% Setting up M=8 channel DFT filter bank 
+M = 8; 
+h0 = [1 1] / sqrt(2); 
+h1 = [1 -1] / sqrt(2); 
+h00 = [1 0 1] / sqrt(2); 
+h10 = [1 0 -1] / sqrt(2); 
+h000 = [1 0 0 0 1] / sqrt(2); 
+h100 = [1 0 0 0 -1] / sqrt(2); 
+
+H_temp = conv(h0, h00); 
+H(1,:) = conv(H_temp, h000); G(1,:) = H(1, :); 
+H(2,:) = conv(H_temp, h100); G(2,:) = -H(2, :); 
+
+H_temp = conv(h0, h10);
+H(3,:) = conv(H_temp, h000); G(3,:) = -H(3, :); 
+H(4,:) = conv(H_temp, h100); G(4,:) = H(4, :); 
+
+H_temp = conv(h1, h00);
+H(5,:) = conv(H_temp, h000); G(5,:) = -H(5, :); 
+H(6,:) = conv(H_temp, h100); G(6,:) = H(6, :); 
+
+H_temp = conv(h1, h10);
+H(7,:) = conv(H_temp, h000); G(7,:) = H(7, :); 
+H(8,:) = conv(H_temp, h100); G(8,:) = -H(8, :); 
