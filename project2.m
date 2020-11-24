8922c41bf17eef3fcb9b8764d1b01dad3b55a88c
Sean Hwang <hwang103@purdue.edu>
2020-11-24 03:55:05 -0500

    add part B

--- project2.m
+++ project2.m
@@ -107,4 +107,76 @@ for m = 1:M
     y = y+Y(m,:);
 end
 
+domega = 2*pi/1024;
+omega = -pi:domega:pi-domega;
 
+yf1 = abs(fftshift(fft(x,1024)));
+yf2 = abs(fftshift(fft(y,1024)));
+
+figure(2) %Figure 1(b) 
+plot(omega, yf1)
+axis([-pi pi 0 max(yf1)]) 
+xlabel('Omega, \omega (rad/sec)');
+ylabel('Magnitude of DTFT') 
+title('Figure 1(b): DTFT of Gaussian random process input signal') 
+grid on 
+
+%iv) DTFT of the corresponding output of the filter 
+
+figure(3) %Figure 1(c) 
+plot(omega, yf2, 'r')
+axis([-pi pi 0 max(yf2)]) 
+xlabel('Omega, \omega (rad/sec)');
+ylabel('Magnitude of DTFT') 
+title('Figure 1(c): DTFT of the Gaussian random process output') 
+grid on 
+
+
+% B) h_0 = h ,, h_1 = (-1)^n * h_0 
+
+N = 16; 
+beta = 0.35; 
+n = -N:(N-1);
+n = n+0.5;
+
+h = 2 * beta * cos((1+beta)*pi*n/2)./(pi*(1-4*beta^2*n.^2));
+h = h + sin((1-beta)*pi*n/2)./(pi*(n-4*beta^2*n.^3));
+h = h * sqrt(2); 
+
+h0 = h;
+h1 = (-1).^(0:(length(n)-1)).*h; 
+h00 = zeros(1,2*length(h)); 
+h10 = h00;
+h00(1,1:2:length(h00)) = h0;
+h10(1,1:2:length(h10)) = h1;
+h000 = zeros(1,4*length(h)); 
+h100 = h000;
+h000(1,1:4:length(h000)) = h0;
+h100(1,1:4:length(h100)) = h1; 
+
+H_tempB = conv(h0, h00); 
+H_B(1,:) = conv(H_tempB, h000); G_B(1,:) = H_B(1, :); 
+H_B(2,:) = conv(H_tempB, h100); G_B(2,:) = -H_B(2, :); 
+
+H_tempB = conv(h0, h10);
+H_B(3,:) = conv(H_tempB, h000); G_B(3,:) = -H_B(3, :); 
+H_B(4,:) = conv(H_tempB, h100); G_B(4,:) = H_B(4, :); 
+
+H_tempB = conv(h1, h00);
+H_B(5,:) = conv(H_tempB, h000); G_B(5,:) = -H_B(5, :); 
+H_B(6,:) = conv(H_tempB, h100); G_B(6,:) = H_B(6, :); 
+
+H_tempB = conv(h1, h10);
+H_B(7,:) = conv(H_tempB, h000); G_B(7,:) = H_B(7, :); 
+H_B(8,:) = conv(H_tempB, h100); G_B(8,:) = -H_B(8, :); 
+
+% i) All corresponding DTFT's h_mB(w) 
+
+h_mB(1,:)=abs(fftshift(fft(H_B(1,:),512)));
+h_mB(2,:)=abs(fftshift(fft(H_B(2,:),512)));
+h_mB(3,:)=abs(fftshift(fft(H_B(3,:),512)));
+h_mB(4,:)=abs(fftshift(fft(H_B(4,:),512)));
+h_mB(5,:)=abs(fftshift(fft(H_B(5,:),512)));
+h_mB(6,:)=abs(fftshift(fft(H_B(6,:),512)));
+h_mB(7,:)=abs(fftshift(fft(H_B(7,:),512)));
+h_mB(8,:)=abs(fftshift(fft(H_B(8,:),512))); 
